c9b5776a5e5a678c4c91443f2ead7ec911b75dde
Anu <anu@gfedev-1429.bom.gov.au>
2019-10-01 14:49:45 +1000

    minor change

--- app/src/main/java/auto/ausiot/util/Constants.java
+++ app/src/main/java/auto/ausiot/util/Constants.java
@@ -41,8 +41,8 @@ public class Constants {
 
     //Connect URL to rest service
 
-    public static String BASE_URL = "http://10.0.2.2:8080";
-    //public static String BASE_URL = "http://146.148.75.238:8080";
+    //public static String BASE_URL = "http://10.0.2.2:8080";
+    public static String BASE_URL = "http://146.148.75.238:8080";
 
 
 
